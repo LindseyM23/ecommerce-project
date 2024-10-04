f8659891b1e34553df0085e2f8057d1aa5d268b1
kevincode06 <113902012+kevincode06@users.noreply.github.com>
2024-10-04 15:20:56 +0200

    link to css

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import '../styles/checkout.css'; 
 
 
 // Main CheckoutPage component
